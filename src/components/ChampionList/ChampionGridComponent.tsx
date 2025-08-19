aa2cb3605b724e4d1d5fce22e15588532844e1d6
WDFAllan <wdfallan@gmail.com>
2025-08-19 18:23:47 +0200

    fix card moving animation

--- src/components/ChampionList/ChampionGridComponent.tsx
+++ src/components/ChampionList/ChampionGridComponent.tsx
@@ -27,7 +27,7 @@ function ChampionGridComponent({ champions, championsHasWon }:Props){
                     initial={{ opacity: 0.5, scale: 0.9, y: 10 }}
                     animate={{ opacity: 1, scale: 1, y: 0 }}
                     exit={{ opacity: 0, scale: 0.9, y: -10 }}
-                    transition={{ duration: 0.15 }}
+                    transition={{ duration: 0.2 }}
                 >
                     <ChampionCardComponent
                         key={champion.name}
